d96b67a526a62f14a4ed37e2f9b4a15682ddf0a2
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-08-10 19:18:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+This will soon be my small projects application
+
+[ ] finish the advice application
+    [x] figure out mobile layout
+        [x] positionining
+        [x] custom colors
+        [x] font
+    [x] setup custom templates
+    [ ] set up on github
+    [ ] set up functionality
+        [ ] find api
+        [ ] api hooking
+        [ ] api calling
+        [ ] button functionality 
+[ ] refactor so that it can allow routing of other react things
+
+
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-dvh bg-blue-950 text-gray-300">
+        <div className="flex justify-center align-middle justify-items-center font-extrabold">
+          <div className="quote-container max-w-lg mt-20 mx-5 p-10 text-center bg-blue-900 rounded-lg">
+            <p className="text-xs mb-10 text-green-300">
+              ADVICE #177
+            </p>
+            <p className="text-xl">
+              "It's easy to sit up and take notice, what's difficult is getting
+              up and taking action."
+            </p> 
+            <div className="flex justify-center">
+            <img className="mt-10" src="src/assets/pattern-divider-mobile.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
