a711b72f19326351d06554a50829c7f6d15a6ed5
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-08-11 17:05:42 +0900

    finished button functionality and positioning

--- src/App.tsx
+++ src/App.tsx
@@ -26,14 +26,14 @@ function App() {
   return (
     <>
       <div className="min-h-dvh bg-blue-950 text-gray-300">
-        <div className="flex justify-center justify-items-center font-extrabold">
-          <div className="quote-container flex-col max-w-lg mx-5 p-10 text-center bg-blue-900 rounded-lg">
+        <div className="flex justify-center justify-items-center">
+          <div className="quote-container relative mt-50 flex-col max-w-lg mx-5 p-10 text-center bg-blue-900 rounded-lg">
             {advice != null ? (
               <>
-                <p className="text-xs my-5 text-green-300">
+                <p className="text-l my-5 text-green-300 font-medium">
                   ADVICE #{advice.id}
                 </p>
-                <p className="text-xl my-8 ">{advice.advice}</p>
+                <p className="text-xl my-8 font-extrabold">{advice.advice}</p>
               </>
             ) : (
               <>
@@ -44,7 +44,9 @@ function App() {
               <img src={quoteImg} />
             </div>
             <button
-              className="p-4 rounded-full bg-green-500"
+              className="filter absolute -bottom-8 left-1/2 -translate-x-1/2  
+              p-4 rounded-full bg-green-500 transition ease-in-out duration-200 
+              hover:brightness-125 hover:drop-shadow-lg active:hue-rotate-90 active:scale-110"
               onClick={() => fetchAdvice()}
             >
               <img src={adviceDice} />
